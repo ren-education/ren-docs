a31d7b3b313f605fbf4fdabe064fb0ff232b66e0
JCSnap <e0970167@u.nus.edu>
2025-07-26 14:17:58 +0800

    change title to ren.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-    title: "Renaissance",
+    title: "Ren.",
     tagline: "Personalised education, at scale",
     favicon: "img/favicon.ico",
 
